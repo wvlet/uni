1480ce850ff4f7e3e3e7b7141fc9677c4efa9cbf
Taro L. Saito <leo@xerial.org>
2026-04-26 22:47:31 -0700

    chore: Remove accidentally-committed .tmp-scratch files

--- .tmp-scratch/TestLocalSimpleName.scala
+++
@@ -1,6 +0,0 @@
-object TestLocalSimpleName:
-  abstract class Animal
-  def main(args:Array[String]): Unit =
-    case class Fish(depth:Int) extends Animal
-    println(classOf[Fish].getSimpleName)
-    println(Fish(1).getClass.getSimpleName)
